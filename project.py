0d211e5fe7b33ea0e17c09bd442827810892af6b
Morgan <m.garricks@lancaster.ac.uk>
2024-10-28 21:11:24 +0000

    Title

--- project.py
+++ project.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import streamlit as st
+#python -m streamlit run project.py
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image(r"images\1.png")
+
+with col2:
+    st.title("My Projects")
+    content = "ALLOT"
+    st.info(content)
