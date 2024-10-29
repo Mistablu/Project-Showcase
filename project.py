0b51c31550c94f1466fc50e619a82d567eaba502
Morgan <m.garricks@lancaster.ac.uk>
2024-10-29 00:07:16 +0000

    added images, links and descriptions

--- project.py
+++ project.py
@@ -3,9 +3,8 @@ import pandas
 #python -m streamlit run project.py
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
-
 with col1:
-    st.image(r"images\1.png")
+    st.image(r"E:\Users\User1\Pictures\Strong Memes\loods\furinne.jpeg")
 
 with col2:
     st.title("My Projects")
@@ -13,11 +12,17 @@ with col2:
     st.info(content)
 st.write("Below you can find some of the apps i have built, please feel free to contact me")
 
-col3, col4 = st.columns(2)
+col3, empty_col,col4 = st.columns([1.5,0.3,1.5])
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
